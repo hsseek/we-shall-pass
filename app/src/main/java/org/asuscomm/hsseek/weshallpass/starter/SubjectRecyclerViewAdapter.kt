143c83a80b12aa75db56380b95d26e8fd42ebeeb
Soldier87 <sunskham@gmail.com>
2019-04-06 19:49:20 +0900

    Add a footer add button

--- app/src/main/java/org/asuscomm/hsseek/weshallpass/starter/SubjectRecyclerViewAdapter.kt
+++ app/src/main/java/org/asuscomm/hsseek/weshallpass/starter/SubjectRecyclerViewAdapter.kt
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
 import android.widget.TextView
 import org.asuscomm.hsseek.weshallpass.R
 
@@ -13,10 +15,13 @@ import org.asuscomm.hsseek.weshallpass.starter.ExamSubjectsFragment.OnSubjectInt
 import kotlinx.android.synthetic.main.fragment_subject_item.view.*
 import org.asuscomm.hsseek.weshallpass.models.Subject
 
+const val VIEW_TYPE_SUBJECT = 1
+const val VIEW_TYPE_FOOTER = 2
+
 class SubjectRecyclerViewAdapter(
     private val subjects: MutableList<Subject>,
     private val mListener: OnSubjectInteractionListener?
-) : RecyclerView.Adapter<SubjectRecyclerViewAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val mOnClickListener: View.OnClickListener
 
@@ -29,24 +34,42 @@ class SubjectRecyclerViewAdapter(
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_subject_item, parent, false)
-        return ViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view: View
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val subject = subjects[position]
-        holder.subjectTitle.text = subject.title
-        holder.subjectDuration.text = subject.duration.toString()
-
-        with(holder.mView) {
-            tag = subject
-            setOnClickListener(mOnClickListener)
+        return if (viewType == VIEW_TYPE_FOOTER) {
+            view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_subject_add, parent, false)
+            FooterViewHolder(view)
+        } else {
+            view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_subject_item, parent, false)
+            SubjectViewHolder(view)
         }
     }
 
-    override fun getItemCount(): Int = subjects.size
+    override fun getItemViewType(position: Int): Int {
+        return if (position == subjects.size) VIEW_TYPE_FOOTER else VIEW_TYPE_SUBJECT
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is FooterViewHolder) { /* The add button row */
+            holder.itemView.setOnClickListener {
+                // TODO: Add a subject
+            }
+        } else if (holder is SubjectViewHolder) { /* Normal subject item */
+            val subject = subjects[position]
+            holder.subjectTitle.text = subject.title
+            holder.subjectDuration.text = subject.duration.toString()
+
+            with(holder.mView) {
+                tag = subject
+                setOnClickListener(mOnClickListener)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = subjects.size + 1
 
     fun replaceSubject(subjects: List<Subject>) {
         this.subjects.clear()
@@ -55,12 +78,12 @@ class SubjectRecyclerViewAdapter(
         }
     }
 
-    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+    inner class SubjectViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val subjectInclude: CheckBox = mView.checkbox_subject_include
         val subjectTitle: TextView = mView.text_subject_title
         val subjectDuration: TextView = mView.edit_subject_duration
-
-        override fun toString(): String {
-            return super.toString() + " '" + subjectDuration.text + "'"
-        }
+        val subjectDelete: ImageView = mView.button_subject_remove
     }
+
+    inner class FooterViewHolder(val mView: View): RecyclerView.ViewHolder(mView)
 }
